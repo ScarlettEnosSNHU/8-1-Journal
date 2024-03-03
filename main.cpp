31b5df35ea69aefeacf360f80aaed0b7e7e88ea8
ScarlettEnosSNHU <161971211+ScarlettEnosSNHU@users.noreply.github.com>
2024-03-03 12:06:05 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include "CornerGrocer.h"
 
+int main() {
+	CornerGrocer cornerGrocer; //Here I made an instance of the Corner Grocer class.
+	cornerGrocer.readInputFile(); //Here I am using the instance and reading the from the input file. 
+	cornerGrocer.writeToFrequencyDataFile(); //Here I am writing to the output file.
+	cornerGrocer.userMenu(); //Here I am using the instance to display the menu.
+
+	return 0;
+}
